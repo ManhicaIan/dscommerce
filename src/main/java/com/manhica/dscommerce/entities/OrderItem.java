586ec46d457f596010fb2c9157eb39961c75ee94
Ian Manhica <ianmanhica4@gmail.com>
2026-02-26 21:23:40 +0200

    Initial clean commit after repository corruption

---
+++ src/main/java/com/manhica/dscommerce/entities/OrderItem.java
@@ -0,0 +1,46 @@
+package com.manhica.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_item_tb")
+public class OrderItem {
+
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+}
