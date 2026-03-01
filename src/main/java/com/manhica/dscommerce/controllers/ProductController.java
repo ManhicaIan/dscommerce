cc98514c89869d8183567c9f6aa6eb2170214a62
Ian Manhica <ianmanhica4@gmail.com>
2026-03-01 14:20:35 +0200

    setting up the correct responses for each resource

--- src/main/java/com/manhica/dscommerce/controllers/ProductController.java
+++ src/main/java/com/manhica/dscommerce/controllers/ProductController.java
@@ -2,10 +2,15 @@ package com.manhica.dscommerce.controllers;
 
 import com.manhica.dscommerce.dto.ProductDTO;
 import com.manhica.dscommerce.services.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -15,17 +20,20 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id){
-        return service.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
     }
 
     @GetMapping
-    public Page<ProductDTO> findAllByPage(Pageable pageable){
-        return service.findAllByPage(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAllByPage(Pageable pageable){
+        return ResponseEntity.ok(service.findAllByPage(pageable));
     }
 
     @PostMapping
-    public ProductDTO insert(@RequestBody ProductDTO dto){
-        return service.insert(dto);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+        ProductDTO insert = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+                buildAndExpand(insert.getId()).toUri();
+        return ResponseEntity.created(uri).body(insert);
     }
 }
